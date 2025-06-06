c10f31a7006bc378af6171aa2c425a704e4d7c4d
AkaraChen <akarachen@outlook.com>
2025-06-06 17:38:47 +0800

    refactor(exec.ts): simplify exec function by removing commented-out code and streamlining feature setup and teardown logic for better readability and maintainability

--- src/lifecycle/exec.ts
+++ src/lifecycle/exec.ts
@@ -11,39 +11,17 @@ export const exec = (
 ) => {
     const { features, mode } = task;
     return Effect.gen(function* () {
-        // const result =
-        //     mode === 'setup'
-        //         ? yield* Effect.forEach(features, (feature) => {
-        //               return Effect.gen(function* () {
-        //                   return yield* feature
-        //                       .setup(ctx, feature.options)
-        //                       .pipe(Effect.withLogSpan(feature.name));
-        //               });
-        //           })
-        //         : yield* Effect.forEach(features, (feature) => {
-        //               return Effect.gen(function* () {
-        //                   if (feature.teardown) {
-        //                       yield* feature
-        //                           .teardown(ctx)
-        //                           .pipe(Effect.withLogSpan(feature.name));
-        //                   }
-        //               });
-        //           });
         const result = yield* Effect.forEach(features, (feature) => {
             return Effect.gen(function* () {
                 switch (mode) {
                     case 'setup':
-                        return yield* feature
-                            .setup(ctx, feature.options)
-                            .pipe(Effect.withLogSpan(feature.name));
+                        return yield* feature.setup(ctx, feature.options);
                     case 'teardown':
                         if (feature.teardown) {
-                            return yield* feature
-                                .teardown(ctx)
-                                .pipe(Effect.withLogSpan(feature.name));
+                            return yield* feature.teardown(ctx);
                         }
                 }
-            });
+            }).pipe(Effect.withLogSpan(feature.name));
         });
         return result.filter((x) => !!x);
     });
